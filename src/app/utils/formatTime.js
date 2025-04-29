2ba86127e1b7ff32ee505f4b627d78efd28a91ae
Nitin <150040530+nitin-is-me@users.noreply.github.com>
2025-04-29 16:31:56 +0530

    Update formatTime.js

--- src/app/utils/formatTime.js
+++ src/app/utils/formatTime.js
@@ -1,3 +1,4 @@
+// Showing time in <time> ago format
 // export function formatTimeAgo(date) {
 //     const now = new Date();
 //     const timeDiff = now - new Date(date);
@@ -9,13 +10,15 @@
 //     const weeks = Math.floor(days/ 7);
 //     const months = Math.floor(days / 30);
 //     const years = Math.floor(days / 365);
-  
+
+//     Option 1: Using full values
+
 //     // if (days > 0) return `${days} day${days > 1 ? 's' : ''} ago`;
 //     // if (hours > 0) return `${hours} hour${hours > 1 ? 's' : ''} ago`;
 //     // if (minutes > 0) return `${minutes} minute${minutes > 1 ? 's' : ''} ago`;
 //     // return `${seconds} second${seconds > 1 ? 's' : ''} ago`;
 
-//     //these returned full values, so i'm using alternate words below
+//     Option 2: Using abbreviations 
 
 //     if(years > 0) return `${years}y ago`;
 //     if(months> 0) return `${months}mo ago`;
