d04355a5b74f832db65953086b3dcf5c06d963c1
Nitin <nitinjha2609@gmail.com>
2025-12-23 17:29:22 +0530

    Update metadata description and add verification

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,10 @@ import "./globals.css";
 import "bootstrap-icons/font/bootstrap-icons.css"
 export const metadata = {
   title: "The Blog Zone",
-  description: "Created by Nitin",
+  description: "An open source and secure blogging platform for your public or private blogs",
+  verification:{
+    google: "OcTLnRMAPQBsCsFlZjz9CV2WL1SNSbk-5aoeqT8uEDs"
+  },
 };
 
 export default function RootLayout({ children }) {
