057453998da4b8277d528ec7c141b4af9f8412c2
Nitin <nitinjha2609@gmail.com>
2025-12-25 13:43:46 +0530

    Refactor metadata for improved title and description
    Updated metadata structure for title and description.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,11 @@
 import "./globals.css";
 import "bootstrap-icons/font/bootstrap-icons.css"
 export const metadata = {
-  title: "The Blog Zone",
-  description: "An open source and secure blogging platform for your public or private blogs",
+  title: {
+    default: "The Blog Zone – Open Source Blogging Platform",
+    template: "%s | The Blog Zone",
+  },
+  description: "The Blog Zone is an open-source, secure blogging platform for writing and public or private blogs with full control.",
   verification:{
     google: "OcTLnRMAPQBsCsFlZjz9CV2WL1SNSbk-5aoeqT8uEDs"
   },
